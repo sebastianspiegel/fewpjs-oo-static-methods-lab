101fafbb0688b9537ed9329029861585392329ed
sebastianspiegel <sebastian.j.spiegel@gmail.com>
2021-02-10 10:56:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9 '-]/g, ''); 
+  }
+
+  static titleize(string){
+    let ignore = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let array = string.split(' ')
+    let newArray = []
+    for ( let i = 0; i < array.length; i++ ){
+      if (i == 0) {
+        newArray.push(this.capitalize( array[i] ))
+      } else {
+        if (ignore.includes( array[i] )) {
+          newArray.push( array[i] )
+        } else {
+          newArray.push( this.capitalize(array[i]) )
+        }
+      }
+    }
+    return newArray.join(" ")
+  }
 }
+
+//capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word.
